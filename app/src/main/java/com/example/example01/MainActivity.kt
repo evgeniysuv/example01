1523bf9d83b6d95353e456411fd64a4f87268c92
Евгений Суворов <evgeniy.se@mail.ru>
2019-11-25 14:53:51 +0300

    CountDownTimer implementation

--- app/src/main/java/com/example/example01/MainActivity.kt
+++ app/src/main/java/com/example/example01/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.example.example01
 
 
-import android.graphics.Color
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.util.concurrent.atomic.AtomicBoolean
 
-const val TAG = "myTag"
+private const val TAG = "myTag"
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -18,6 +19,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var button02: Button
     private lateinit var button03: Button
     private lateinit var textView: TextView
+    private lateinit var counterView: TextView
+    private lateinit var timeRemainingTextView: TextView
+    private lateinit var timer: CountDownTimer
+
+    private var counter = 0
+    private var initialCountDown: Long = 10000
+    private var countDownInterval: Long = 1000
+
+    private var isStarted: AtomicBoolean = AtomicBoolean(false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +35,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button01 = findViewById(R.id.activity_main__push_me_button01)
         button02 = findViewById(R.id.activity_main__push_me_button02)
         button03 = findViewById(R.id.activity_main__push_me_button03)
-        textView = findViewById(R.id.textView)
+        textView = findViewById(R.id.exampleTextView)
+        counterView = findViewById(R.id.counterView)
+        timeRemainingTextView = findViewById(R.id.timeRemainingTextView)
+
+        counterView.text = getString(R.string.text_counter, counter)
+        timeRemainingTextView.text =
+            getString(R.string.time_remaining, initialCountDown / 1000, " сек")
 
         button01.setOnClickListener(this)
         button02.setOnClickListener(this)
@@ -34,12 +50,29 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         Log.d(TAG, "Application initialized")
     }
 
+    private fun initTimer() {
+        timeRemainingTextView.text =
+            getString(R.string.time_remaining, initialCountDown / 1000, " сек")
+        timer = object : CountDownTimer(initialCountDown, countDownInterval) {
+
+            override fun onFinish() {
+                textView.text = "Time is over"
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                timeRemainingTextView.text =
+                    getString(R.string.time_remaining, millisUntilFinished / 1000, " сек")
+            }
+        }
+    }
+
     override fun onStart() {
         super.onStart()
         Log.d("Test", "Main activity started")
     }
 
     override fun onClick(v: View?) {
+        startGame()
         var toast: Toast? = null
         when (v?.id) {
             R.id.activity_main__push_me_button01 -> {
@@ -58,7 +91,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 toast = Toast.makeText(this, "Button 3 pushed successfully", Toast.LENGTH_LONG)
             }
         }
-        toast?.view?.setBackgroundColor(Color.GREEN)
+        incrementCounter()
         toast?.show()
+    }
+
+    private fun startGame() {
+        if (!isStarted.get()) {
+            isStarted.set(true)
+            initTimer()
+            timer.start()
+            Toast.makeText(this, "Game started", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun incrementCounter() {
+        val newCounter = getString(R.string.text_counter, ++counter)
+        counterView.text = newCounter
+
     }
 }
